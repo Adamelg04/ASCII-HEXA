ea6028fb435bd3a91e6f40d1dd6976a971774158
adame <adame@DESKTOP-VOPV20U>
2022-02-06 14:55:12 +0100

    Přidejte soubory projektu.

---
+++ ASCII-HEXA.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+
+int main() {
+	FILE* soubor = fopen("Tabulka.txt", "w");
+
+	if (soubor == NULL) {
+		printf("Error: Soubor nelze otevrit pro zapis.\n");
+		return 1;
+	}
+
+	int ASCII = 0x40;
+
+	for (int i = 0; i <26 ; i++) {
+		ASCII = ASCII + 1;
+		fwrite(&ASCII, sizeof(int), 1, soubor);
+		fprintf(soubor, "Hexadecimálně: %x\n", ASCII);
+	}
+
+	if (fclose(soubor) == EOF) {
+		printf("Error: Soubor se nepodarilo otevrit.\n");
+		return 1;
+	}
+
+	return(0);
+}
